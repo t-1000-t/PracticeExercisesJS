67ad0db3aa9335311704800619104fd6dcb9bc7a
vlad <goognya@gmail.com>
2019-06-05 21:50:38 +0300

    practice 2 ready

---
+++ PrExJs2/main.js
@@ -0,0 +1,10 @@
+const day = 17;
+const month = 10;
+const year = 2048;
+
+const DATE = `${day}\\${month}\\${year}`;
+
+const MESSEGE = `"Доброе утро, cегодня ${DATE}, за бортом отличная погода!"`;
+
+console.log(DATE);
+console.log(MESSEGE);
