b8c83966243983d0fd2dc54227c0e0f4ea3b6135
vlad <goognya@gmail.com>
2019-06-13 13:32:43 +0300

    practise 2 ready

---
+++ Modul2/PrExJs2/main.js
@@ -0,0 +1,6 @@
+const users = ['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong'];
+let value;
+for (let i = 0; i < users.length; i += 1) {
+  value = users[i];
+  console.log(`Номер елемента: ${i + 1}, значение елемента: ${value}`);
+}
