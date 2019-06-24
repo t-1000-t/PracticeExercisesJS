82a23c94c4b10f200b37ea34550d8eb1a60fec0d
vlad <goognya@gmail.com>
2019-06-24 15:28:21 +0300

    Module 3 Practise 5 correct

--- Module3/PrExJs5/main.js
+++ Module3/PrExJs5/main.js
@@ -1,20 +1,16 @@
 // eslint-disable-next-line consistent-return
 function findLongestWord(str) {
   const arr = str.split(' ');
-  // eslint-disable-next-line prefer-const
   let longestWord = '';
-  let result = '';
   for (let i = 0; arr.length > i; i += 1) {
-    longestWord = longestWord.length > arr[i].length ? longestWord : arr[i];
-    result = longestWord;
-    return result;
-    // if (arr[i].length > longestWord.length) {
-    //   longestWord = arr[i];
-    //   result = longestWord;
-    //   return result;
-    // }
+    // longestWord = longestWord.length > arr[i].length ? longestWord : arr[i];
+    if (arr[i].length > longestWord.length) {
+      longestWord = arr[i];
+    }
   }
-  return result;
+  return longestWord;
 }
 
 console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+console.log(findLongestWord('Google do a roll')); // 'Google'
+console.log(findLongestWord('May the force be with you')); // 'force'
