55410ce51385b130a52c70e89136d7675dcdf0e6
vlad <goognya@gmail.com>
2019-07-07 22:19:34 +0300

    Module 5 Practise 3

---
+++ Module5/PrExJs3/main.js
@@ -0,0 +1,34 @@
+/*
+  Напиши функцию-конструкор User для создания
+  пользователя со следующим свойствами:
+    - name - строка (имя)
+    - age - число (возраст)
+    - friends - число (кол-во друзей)
+
+  Имя, активность, возраст и друзей,
+  будут переданы при вызове конструктора User.
+
+  Добавь метод getInfo(), который, выводит строку:
+  `User ${имя} is ${возраст} years old and has ${кол-во друщзей} friends`
+*/
+
+const mango = new User({
+  name: 'Mango',
+  age: 2,
+  friends: 20,
+});
+
+mango.getInfo(`User ${name} is ${age} years old and has ${friends} friends`);
+
+
+// User Mango is 2 years old and has 20 friends
+
+const poly = new User({
+  name: 'Poly',
+  age: 3,
+  friends: 17,
+});
+
+poly.getInfo(`User ${name} is ${age} years old and has ${friends} friends`);
+
+// User Poly is 3 years old and has 17 friends
