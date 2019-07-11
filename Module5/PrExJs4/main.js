7cdb73a8d34d632591d5f2d374a4ecdac0e24c1c
vlad <goognya@gmail.com>
2019-07-11 18:04:04 +0300

    Module 5 Prectise 4

---
+++ Module5/PrExJs4/main.js
@@ -0,0 +1,44 @@
+/*
+ * Напиши конструктор Storage(items), который будет создавать объекты для управления складом товаров
+ * При вызове будет получать один аргумент - начальный массив товаров, и записываеть его в свойство items
+ *
+ * Добавь метод getItems, который возвращает массив текущих товаров
+ * Добавь метод addItem(item), который получает новый товар и добавляет его к текущим
+ * Добавь метод removeItem(item), который получет товар и, если он есть, удаляет его из текущих
+*/
+
+
+function Storage(items) {
+  this.items = items;
+  this.getItems = function () {
+    return items;
+  };
+  this.addItem = function (item) {
+    if (!items.includes(item)) {
+      return items.push(item);
+    }
+  };
+  this.removeItem = function (item) {
+    let index = 0;
+    if (items.includes(item)) {
+      index = items.findIndex(elem => elem === item);
+      return items.splice(index, 1);
+    }
+  };
+}
+
+const storage = new Storage([
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+]);
+
+const items = storage.getItems();
+console.log(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+storage.addItem('Дроид');
+console.log(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem('Пролонгер');
+console.log(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
