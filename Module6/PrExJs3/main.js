d147d30d02a551d09cd733d202338f1845ee83e1
vlad <goognya@gmail.com>
2019-07-25 15:40:27 +0300

    Module 6 Practise 3

---
+++ Module6/PrExJs3/main.js
@@ -0,0 +1,55 @@
+class Car {
+  constructor({
+    maxSpeed = 0, speed = 0, running, distance = 0,
+  }) {
+    this.maxSpeed = maxSpeed;
+    this.speed = speed;
+    this.running = running;
+    this.distance = distance;
+    /*
+      Добавь свойства:
+        - speed - для отслеживания текущей скорости, изначально 0.
+        - maxSpeed - для хранения максимальной скорости
+        - running - для отслеживания заведен ли автомобиль, возможные значения true или false. Изначально false.
+        - distance - содержит общий киллометраж, изначально с 0
+    */
+  }
+
+  turnOn() {
+    this.running = true;
+    // Добавь код для того чтобы завести автомобиль
+    // Просто записывает в свойство running значание true
+  }
+
+  turnOff() {
+    this.running = false;
+    // Добавь код для того чтобы заглушить автомобиль
+    // Просто записывает в свойство running значание false
+  }
+
+  accelerate(spd) {
+    if (this.speed < this.maxSpeed) {
+      this.speed = spd;
+    }
+    // Записывает в поле speed полученное значение, при условии что
+    // оно не больше чем значение свойства maxSpeed
+  }
+
+  decelerate(spd) {
+    if (this.speed < this.maxSpeed && this.speed > 0) {
+      this.speed = spd;
+    }
+    // Записывает в поле speed полученное значение, при условии что
+    // оно не больше чем значение свойства maxSpeed и не меньше нуля
+  }
+
+  drive(hours) {
+    if (this.turnOn()) {
+      this.distance = this.speed * hours;
+    }
+    // Добавляет в поле distance киллометраж (hours умноженное на значение поля speed),
+    // но только в том случае если машина заведена!
+  }
+}
+
+const car = new Car({ maxSpeed: 100 });
