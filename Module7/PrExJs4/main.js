f1541def0c9d20dbafb9cd7cb87cf0e0894cf377
vlad <goognya@gmail.com>
2019-07-28 21:46:43 +0300

    Module 7 Practise 4

---
+++ Module7/PrExJs4/main.js
@@ -0,0 +1,21 @@
+/*
+  Напиши функию getActiveGuests(guests), принимающую массив объектов гостей.
+
+  Функция должна возвращать массив объектов гостей, значение поля isActive которых true.
+
+  PS: обязательно используй перебирающие методы массивов, никаких for!
+*/
+
+const guests = [
+  { name: 'Mango', age: 20, isActive: true },
+  { name: 'Poly', age: 18, isActive: false },
+  { name: 'Ajax', age: 30, isActive: true },
+  { name: 'Chelsey', age: 45, isActive: false },
+];
+
+function getActiveGuests(guests) {
+  return guests.filter(guest => guest.isActive === true);
+}
+
+// Вызовы функции для проверки
+console.log(getActiveGuests(guests)); // массив из 2-х объектов Mango и Ajax
