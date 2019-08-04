f54402aff418d1d895620e8fa9c1886a16aea1ee
vlad <goognya@gmail.com>
2019-08-04 15:52:26 +0300

    Module 8 Practise 1 new

--- Module8/PrExJs1/main.js
+++ Module8/PrExJs1/main.js
@@ -1,109 +1,93 @@
-
 const namesAnimals = ['Кот', 'Хомяк', 'Лошадь', 'Попугай'];
 
-const createlistItem = (section, arr) => {
+const createlistItem = (arr) => {
   const list = document.createElement('ul');
-
-  list.textContent = section;
-  list.classList.add('list');
 
   const listLi = document.createElement('li');
   listLi.textContent = arr[0];
-  listLi.classList.add('clsItem');
+
+  const listLi1 = document.createElement('li');
+  listLi1.textContent = arr[1];
 
   const listLi2 = document.createElement('li');
-  listLi2.textContent = arr[1];
-  listLi2.classList.add('clsItem');
+  listLi2.textContent = arr[2];
 
   const listLi3 = document.createElement('li');
-  listLi3.textContent = arr[2];
-  listLi3.classList.add('clsItem');
-
-  const listLi4 = document.createElement('li');
-  listLi4.textContent = arr[3];
-  listLi4.classList.add('clsItem');
+  listLi3.textContent = arr[3];
 
   list.appendChild(listLi);
+  list.appendChild(listLi1);
   list.appendChild(listLi2);
   list.appendChild(listLi3);
-  list.appendChild(listLi4);
 
   return list;
 };
 
-const item = createlistItem('Животные', namesAnimals);
-const list = document.querySelector('body');
+const item = createlistItem(namesAnimals);
+const list = document.querySelector('.clsItem');
 list.appendChild(item);
-/* End 1 */
+/* 1 */
+
 const namesVegetables = ['Хлеб', 'Творог', 'Зелень'];
 
-const createlistItem2 = (section, arr) => {
-  const list2 = document.createElement('ul');
-
-  list2.textContent = section;
-  list2.classList.add('list');
+const createlistItem1 = (arr) => {
+  const list1 = document.createElement('ul');
 
   const listLi = document.createElement('li');
   listLi.textContent = arr[0];
-  listLi.classList.add('clsItem');
+
+  const listLi1 = document.createElement('li');
+  listLi1.textContent = arr[1];
 
   const listLi2 = document.createElement('li');
-  listLi2.textContent = arr[1];
-  listLi2.classList.add('clsItem');
+  listLi2.textContent = arr[2];
+
+
+  list1.appendChild(listLi);
+  list1.appendChild(listLi1);
+  list1.appendChild(listLi2);
+
+
+  return list1;
+};
+
+const item1 = createlistItem1(namesVegetables);
+const list1 = document.querySelector('.clsItem1');
+list1.appendChild(item1);
+/* 2 */
+
+const namesTechnologies = ['html', 'css', 'js', 'react', 'node'];
+
+const createlistItem2 = (arr) => {
+  const list2 = document.createElement('ul');
+
+  const listLi = document.createElement('li');
+  listLi.textContent = arr[0];
+
+  const listLi1 = document.createElement('li');
+  listLi1.textContent = arr[1];
+
+  const listLi2 = document.createElement('li');
+  listLi2.textContent = arr[2];
 
   const listLi3 = document.createElement('li');
-  listLi3.textContent = arr[2];
-  listLi3.classList.add('clsItem');
+  listLi3.textContent = arr[3];
+
+  const listLi4 = document.createElement('li');
+  listLi4.textContent = arr[4];
+
 
   list2.appendChild(listLi);
+  list2.appendChild(listLi1);
   list2.appendChild(listLi2);
   list2.appendChild(listLi3);
+  list2.appendChild(listLi4);
+
 
   return list2;
 };
 
-const item2 = createlistItem2('Продукты', namesVegetables);
-const list2 = document.querySelector('body');
-list.appendChild(item2);
-/* End 2 */
-const namesTechnologies = ['html', 'css', 'js', 'react', 'node'];
-
-const createlistItem3 = (section, arr) => {
-  const list3 = document.createElement('ul');
-
-  list3.textContent = section;
-  list3.classList.add('list');
-
-  const listLi = document.createElement('li');
-  listLi.textContent = arr[0];
-  listLi.classList.add('clsItem');
-
-  const listLi2 = document.createElement('li');
-  listLi2.textContent = arr[1];
-  listLi2.classList.add('clsItem');
-
-  const listLi3 = document.createElement('li');
-  listLi3.textContent = arr[2];
-  listLi3.classList.add('clsItem');
-
-  const listLi4 = document.createElement('li');
-  listLi4.textContent = arr[3];
-  listLi4.classList.add('clsItem');
-
-  const listLi5 = document.createElement('li');
-  listLi5.textContent = arr[4];
-  listLi5.classList.add('clsItem');
-
-  list3.appendChild(listLi);
-  list3.appendChild(listLi2);
-  list3.appendChild(listLi3);
-  list3.appendChild(listLi4);
-  list3.appendChild(listLi5);
-
-  return list3;
-};
-
-const item3 = createlistItem3('Технологии', namesTechnologies);
-const list3 = document.querySelector('body');
-list3.appendChild(item3);
-/* End 3 */
+const item2 = createlistItem2(namesTechnologies);
+const list2 = document.querySelector('.clsItem2');
+list2.appendChild(item2);
+/* 3 */
