122717cd1ba6a21c027a31fd77e2ec21aeb27867
vlad <goognya@gmail.com>
2019-08-19 00:02:32 +0300

    Module 9 Practice 2 ready

--- Module9/PrExJs2/main.js
+++ Module9/PrExJs2/main.js
@@ -3,10 +3,6 @@ const numInputs1 = document.querySelector('.expression > input');
 const btnSum = document.querySelector('button');
 const resultSum = document.querySelector('.result');
 
-// console.log(numInputs0.value);
-// console.log(numInputs1);
-// console.log(btnSum);
-// console.log(resultSum);
 function hendlerBtnSum() {
   const	firstInput = numInputs0.value;
   const secondInput = numInputs1.nextElementSibling.nextElementSibling.value;
