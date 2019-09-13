28ee5d41f47070dadb310da8eff8011e0eabbe1c
vlad <goognya@gmail.com>
2019-09-13 15:27:19 +0300

    Module 12 Practice 3 Promise then

---
+++ Module12/PrExJs3/src/js/app.js
@@ -0,0 +1,13 @@
+const logger = time => console.log(`Resolved after ${time}ms`);
+const delay = (time) => {
+  const promise = new Promise(() => {
+    setInterval(() => {
+      logger(time);
+    }, time);
+  });
+  return promise;
+};
+// Вызовы функции для проверки
+delay(2000).then(logger); // Resolved after 2000ms
+delay(1000).then(logger); // Resolved after 1000ms
+delay(1500).then(logger); // Resolved after 1500ms
